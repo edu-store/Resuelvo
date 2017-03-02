1519759d9124a5c9745be55da8027f4d5e28ce02
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-03-02 10:56:37 -0600

    matriz numeros decimales completada

--- js/decimales.js
+++ js/decimales.js
@@ -34,6 +34,76 @@ define(function (require) {
             'operacion' : [ '168.50', '-', '114.60', '=', '53.90' ],
             'respuesta' : '53.90',
             'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'Sonia decide bordar un mantel de 3 m de largo. Si ya ha bordado 1.82 m de largo, ¿cuántos metros de largo le falta a Sonia para terminar de bordar el mantel?',
+            'datos'     : [ '3', '11.82' ],
+            'operacion' : [ '3', '-', '1.82', '=', '1.18' ],
+            'respuesta' : '1.18',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'En un día se vendieron 13.25 libras de queso en la pulpería de don José. ¿Cuánto dinero recibió don José por la venta si cada libra vale C$ 15.00?',
+            'datos'     : [ '13.25', '15' ],
+            'operacion' : [ '15', '*', '15', '=', '178.95' ],
+            'respuesta' : '178.95',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Si con 5.75 galones de pintura se pinta una casa. ¿Cuántos galones se necesitan para pintar 9.00 casas iguales?',
+            'datos'     : [ '5.75', '9' ],
+            'operacion' : [ '5.75', '*', '9', '=', '51.75' ],
+            'respuesta' : '51.75',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Si cada manzana de café tecnificado puede producir 36.75 quintales del grano de oro como promedio, ¿cuántos quintales producirán 36.50 manzanas?',
+            'datos'     : [ '36.75', '36.5' ],
+            'operacion' : [ '36.75', '*', '36.5', '=', '1341.375' ],
+            'respuesta' : '1341.375',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Don Noé plantó un pino, justo en medio de dos robles que están separados a una distancia de 12.82m entre sí. Calcule la distancia entre el pino y uno de los robles.',
+            'datos'     : [ '12.82', '2' ],
+            'operacion' : [ '12.82', '/', '2', '=', '6.41' ],
+            'respuesta' : '6.41',
+            'signo'     : '/'
+        },
+         {
+            'ejercicio' : 'Pepa salió a comprar material escolar. Como necesitaba cuadernos y le gustaban los 5 colores que había, compró uno de cada color. Cada cuaderno costaba 35.75 córdobas. ¿Cuánto dinero se gastó en los 5 cuadernos?',
+            'datos'     : [ '35.75', '5' ],
+            'operacion' : [ '35.75', '*', '5', '=', '178.75' ],
+            'respuesta' : '178.75',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Tengo C$ 975.90 y quiero repartirlo en partes iguales a tres niños. ¿Cuánto dinero debo dar a cada niño?',
+            'datos'     : [ '975.9', '3' ],
+            'operacion' : [ '975.9', '/', '3', '=', '325.30' ],
+            'respuesta' : '325.30',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Para distribuir 175.5 litros de aceite en botellas con capacidad de 1.5 litros. ¿Cuántas botellas necesitaré?',
+            'datos'     : [ '175.5', '1.5' ],
+            'operacion' : [ '175.5', '/', '1.5', '=', '117' ],
+            'respuesta' : '117',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'María y Vanesa son vecinas, y todas las semanas van juntas de compra. La semana pasada María gastó 450.75 córdobas, exactamente 3 veces más de lo que gastó Vanesa. ¿Cuánto dinero se gastó Vanesa?',
+            'datos'     : [ '450.75', '3' ],
+            'operacion' : [ '450.75', '/', '3', '=', '150.25' ],
+            'respuesta' : '150.25',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Pedro fue a la pulpería y compró 5 libras de azúcar a un costo de 10.50 córdobas por libras. ¿Cuánto dinero gastó?',
+            'datos'     : [ '10.5', '5' ],
+            'operacion' : [ '10.5', '*', '5', '=', '52.50' ],
+            'respuesta' : '52.50',
+            'signo'     : '*'
         }
     ];
 });
