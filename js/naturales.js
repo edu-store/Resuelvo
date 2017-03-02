8a7cd8f575a11472f0408dedc03890bccd0d9fcb
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-03-02 10:15:53 -0600

    matriz numeros naturales completada

--- js/naturales.js
+++ js/naturales.js
@@ -34,6 +34,149 @@ define(function (require) {
             'operacion' : [ '335', '-', '187', '=', '148' ],
             'respuesta' : '148',
             'signo'     : '-'
+        },
+         {
+            'ejercicio' : 'En el jardín de Andrés hay 45 rosales, pero tiene 18 rosales más que en el jardín de Maite. ¿Cuántos rosales tiene el jardín de Maite?',
+            'datos' : [ '45', '18' ],
+            'operacion' : [ '45', '-', '18', '=', '27' ],
+            'respuesta' : '27',
+            'signo'     : '-'
+        },
+         {
+            'ejercicio' : 'Estoy leyendo un libro que tiene 97 páginas y he leído 23 páginas menos de las que tiene. ¿Cuántas páginas he leído? ',
+            'datos' : [ '97', '23' ],
+            'operacion' : [ '97', '-', '23', '=', '74' ],
+            'respuesta' : '74',
+            'signo'     : '-'
+        },
+         {
+            'ejercicio' : 'En un jardín se plantan 78 geranios, pero se marchitan 35. ¿Cuántos quedan?',
+            'datos' : [ '78', '35' ],
+            'operacion' : [ '78', '-', '35', '=', '43' ],
+            'respuesta' : '43',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : ' De los 5,284 huevos incubados salieron 4,615 polluelos. ¿Cuántos huevos se estropearon?',
+            'datos' : [ '5284', '4615' ],
+            'operacion' : [ '5284', '-', '4615', '=', '669' ],
+            'respuesta' : '669',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'María tiene 53 años menos que su abuelo. Si su abuelo tiene 72 años, ¿cuántos años tiene María?',
+            'datos' : [ '72', '53' ],
+            'operacion' : [ '72', '-', '53', '=', '19' ],
+            'respuesta' : '19',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'Un autobús realizó 4 viajes en un día. En cada viaje transportó a 45 pasajeros. Si cada pasaje vale 10 córdobas, ¿cuál fue el ingreso del día?',
+            'datos' : [ '4', '45','10' ],
+            'operacion' : [ '4', '*', '45', '*', '10','=','1800' ],
+            'respuesta' : '19800',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'En un almacén empaquetan latas de sardinas de cuatro en cuatro. ¿Cuántas latas hay en total en 92 paquetes?',
+            'datos' : [ '4', '92' ],
+            'operacion' : [ '4', '*', '92','=','368' ],
+            'respuesta' : '368',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Un ramo tiene 35 rosas. Si cada rosa vale 4 córdobas, ¿cuánto vale el ramo?',
+            'datos' : [ '9', '3' ],
+            'operacion' : [ '9', '*', '3','=','27' ],
+            'respuesta' : '27',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Un ciclista ha recorrido 9 km. y otro una distancia que es el triple de la anterior. ¿Cuántos km. ha recorrido el segundo ciclista? ',
+            'datos' : [ '35', '4' ],
+            'operacion' : [ '35', '*', '4','=','140' ],
+            'respuesta' : '140',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'En una pajarería hay 252 canarios. Los colocamos en jaulas de 6 en 6 ¿Cuántas jaulas serán necesarias? ',
+            'datos' : [ '252', '6' ],
+            'operacion' : [ '252', '%', '6','=','42' ],
+            'respuesta' : '42',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Entre 8 ganaderos han enviado al matadero por igual cantidad 600 reses. ¿Cuántas rese habrá enviado cada uno?',
+            'datos' : [ '600', '8' ],
+            'operacion' : [ '600', '%', '8','=','75' ],
+            'respuesta' : '75',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Un barco sale de puerto Corinto cada 4 días. ¿Cuántas veces habrá salido en 192 días?',
+            'datos' : [ '192', '4' ],
+            'operacion' : [ '192', '%', '4','=','48' ],
+            'respuesta' : '48',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Un coleccionista pega 8 sellos en cada hoja de su álbum. ¿Cuántas hojas necesitará para guardar los 96 sellos que tiene?',
+            'datos' : [ '96', '8' ],
+            'operacion' : [ '96', '%', '8','=','12' ],
+            'respuesta' : '12',
+            'signo'     : '%'
+        },
+          {
+            'ejercicio' : 'El capitán de una compañía de 162 soldados quiere formar filas de 9 soldados. ¿Cuántas filas podrá formar?',
+            'datos' : [ '162', '9' ],
+            'operacion' : [ '162', '%', '9','=','18' ],
+            'respuesta' : '12',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Con 950 vacas hemos formado 5 rebaños. ¿Cuántas vacas tienen cada rebaño?',
+            'datos' : [ '950', '5' ],
+            'operacion' : [ '950', '%', '5','=','190' ],
+            'respuesta' : '190',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Un granjero ha recogido 1,872 huevos de gallina. ¿Cuántas docenas ha recogido?',
+            'datos' : [ '1872', '12' ],
+            'operacion' : [ '1872', '%', '12','=','156' ],
+            'respuesta' : '156',
+            'signo'     : '%'
+        },
+        {
+            'ejercicio' : 'Un panadero ha hecho 4,104 rosquillas. Para su venta las empaqueta en cajas de 2 docenas. ¿Cuántas cajas puede llenar?',
+            'datos' : [ '4104', '24' ],
+            'operacion' : [ '4104', '%', '24','=','171' ],
+            'respuesta' : '171',
+            'signo'     : '%'
+        },
+         {
+            'ejercicio' : 'Samuel compró 8 córdobas de caramelos con un billete de 50 córdobas. ¿Cuánto es el cambio que le corresponde a Samuel?',
+            'datos' : [ '50', '8' ],
+            'operacion' : [ '4104', '-', '24','=','171' ],
+            'respuesta' : '171',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'En una hacienda peren 156 crias, 75 son machos. ¿Cuál es la cantidad de hembras?',
+            'datos' : [ '156', '75' ],
+            'operacion' : [ '156', '-', '75','=','81' ],
+            'respuesta' : '81',
+            'signo'     : '-'
+        },
+         {
+            'ejercicio' : 'Ricardo sacó en el examen de matematicas 95 puntos y Carlos obtuvo 83. ¿Cuántos puntos sacó de más Ricardo?',
+            'datos' : [ '95', '83' ],
+            'operacion' : [ '95', '-', '83','=','12' ],
+            'respuesta' : '12',
+            'signo'     : '-'
         }
+        
+	 
+	 
     ];
 });
