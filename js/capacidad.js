91e070b019a705e22f7d7d3ff6ccb057c7bed3af
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-03-02 12:39:40 -0600

    matriz completada de capacidad

--- js/capacidad.js
+++ js/capacidad.js
@@ -31,9 +31,79 @@ define(function (require) {
         {
             'ejercicio' : 'Una bañera tiene una capacidad de 248 litros. Quieres llenarla con un cubo de 8 litros. ¿Cuántos cubos necesitarás?',
             'datos'     : [ '248', '8' ],
-            'operacion' : [ '248', '/;', '8', '=', '31' ],
+            'operacion' : [ '248', '/', '8', '=', '31' ],
             'respuesta' : '31',
             'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Un tanque tiene una capacidad de 50 litros. ¿Cuántas botellas de 2 litros puedes llenar con lo que cabe en el tanque?',
+            'datos'     : [ '50', '2' ],
+            'operacion' : [ '50', '/', '2', '=', '50' ],
+            'respuesta' : '50',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'El depósito de gasolina de un carro tiene una capacidad de 45 litros. Si tiene en este momento 12 litros, ¿cuántos litros necesitamos para llenarlo?',
+            'datos'     : [ '45', '12' ],
+            'operacion' : [ '45', '*', '12', '=', '33' ],
+            'respuesta' : '33',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'En un almacén hay 25 barriles de vino. Si en cada barril caben 8 litros, ¿cuántos litros de vino hay en el almacén?',
+            'datos'     : [ '25', '8' ],
+            'operacion' : [ '25', '*', '8', '=', '200' ],
+            'respuesta' : '200',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'La familia de Jaime está formada por 4 personas. Cada una gasta diariamente 8 litros de agua en su aseo personal. ¿Cuántos litros de agua gasta la familia en un mes de 30 días?',
+            'datos'     : [ '30', '4','8' ],
+            'operacion' : [ '30', '*', '4', '*','8','=', '960' ],
+            'respuesta' : '960',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : '¿Cuántos cuartos de litro necesitamos para llenar un depósito de 6 litros?',
+            'datos'     : [ '6', '0.25'],
+            'operacion' : [ '6', '/', '0.25','=', '24' ],
+            'respuesta' : '24',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Si en un balde caben 12 litros de leche ¿Cuántos baldes necesito para almacenar 144 litros?',
+            'datos'     : [ '144', '12'],
+            'operacion' : [ '144', '/', '12','=', '12' ],
+            'respuesta' : '12',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'En un barril caben 200 litros de agua ¿Cuántos barriles se necesitan para almacenar 1,600 litros?',
+            'datos'     : [ '1.6', '200'],
+            'operacion' : [ '144', '/', '200','=', '8' ],
+            'respuesta' : '8',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Un pueblo tiene instalado un depósito de 5,000 galones para almacenar agua en época de sequía, si se consumen 100 galones por día ¿En cuántos días se consume todo el depósito?',
+            'datos'     : [ '5', '100'],
+            'operacion' : [ '5000', '/', '100','=', '50' ],
+            'respuesta' : '50',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Juan tiene 12 vasos de refresco de medio litro cada uno ¿Cuántos litros tiene en total?',
+            'datos'     : [ '0.5', '12'],
+            'operacion' : [ '0.5', '*', '12','=', '6' ],
+            'respuesta' : '6',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Don Ramón requiere darle agua a sus 14 vacas, cada vaca consume 5 galones de agua ¿Cuántos galones necesita?',
+            'datos'     : [ '14', '5'],
+            'operacion' : [ '14', '*', '5','=', '70' ],
+            'respuesta' : '50',
+            'signo'     : '*'
         }
     ];
 });
