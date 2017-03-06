abfaf4d6c1e6c9a6ae26aea10bc1646758ec37e7
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-03-06 10:56:15 -0600

    matriz

--- js/fraccionarios.js
+++ js/fraccionarios.js
@@ -2,37 +2,44 @@ define(function (require) {
     return [
         {
             'ejercicio' : ' El día lunes Enrique estudió 1/4 de hora, el martes 3/4 de hora y el miércoles 2/4 horas. ¿Cuántas horas ha estudiado Enrique?',
-            'datos'     : [ '1/4', '3/4', '2/4' ],
+            'datos'     : [ '1/4 hora', '3/4 hora', '2/4 hora' ],
             'operacion' : [ '1/4', '+', '3/4', '+', '2/4', '=', '6/4' ],
-            'respuesta' : '6/4',
+            'respuesta' : 'Ha estudiado 6/4 de hora',
             'signo'     : '+'
         },
         {
             'ejercicio' : 'En una pintura 3/10 de las figuras son reses y 1/10 de las figuras son peces, ¿Qué parte de la pintura muestra a ambos?',
-            'datos'     : [ '3/10', '1/10' ],
+            'datos'     : [ '3/10 reses', '1/10 peces' ],
             'operacion' : [ '3/10', '+', '1/10', '=', '4/10' ],
-            'respuesta' : '4/10',
+            'respuesta' : 'La pintura 4/10',
             'signo'     : '+'
         },
         {
             'ejercicio' : 'Una caja tiene 60 bombones. Eva comió 1/3 y Ana 2/3 ¿Cuánto se comieron entre las 2?',
-            'datos'     : [ '1/3', '2/3' ],
+            'datos'     : [ '1/3 bombones', '2/3 bombones' ],
             'operacion' : [ '1/3', '+', '2/3', '=', '3/3' ],
-            'respuesta' : '3/3',
+            'respuesta' : 'Comieron 3/3 de bombones',
             'signo'     : '+'
         },
         {
             'ejercicio' : 'Don Juan sembró un huerto, en el terreno sembro 1/4 de sandía y 1/4 de melón ¿Cúanto le quedó sin sembrar?',
-            'datos'     : [ '1/4', '1/4' ],
+            'datos'     : [ '1/4 sandia', '1/4 melón' ],
             'operacion' : [ '1/4', '+', '1/4', '=', '2/4' ],
-            'respuesta' : '2/4',
+            'respuesta' : 'Le quedo 2/4 por sembrar',
             'signo'     : '+'
         },
         {
             'ejercicio' : 'Doña María tiene 5/9 de una caja de chocolates y le da a su hija Elizabeth 2/9. ¿Cuánto le queda?',
-            'datos'     : [ '5/9', '2/9' ],
+            'datos'     : [ '5/9 caja', '2/9 caja' ],
             'operacion' : [ '5/9', '-', '2/9', '=', '3/9' ],
-            'respuesta' : '3/9',
+            'respuesta' : 'Le queda 3/9 de caja con chocolate',
+            'signo'     : '-'
+        },
+         {
+            'ejercicio' : 'El cabello de Rosa medía 2/3 de un metro, en el salón de belleza le cortaron 1/3. ¿Qué tan largo es el cabello de Rosa ahora? ',
+            'datos'     : [ '2/3 metro', '1/3 metro' ],
+            'operacion' : [ '2/3', '-', '1/3', '=', '3/9' ],
+            'respuesta' : 'Le queda 3/9 de caja con chocolate',
             'signo'     : '-'
         }
     ];
