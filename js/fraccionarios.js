be9f9288ca21fd61f76129cb30bf257dab177b86
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-03-06 13:08:34 -0600

    matriz de fraccion

--- js/fraccionarios.js
+++ js/fraccionarios.js
@@ -38,9 +38,79 @@ define(function (require) {
          {
             'ejercicio' : 'El cabello de Rosa medía 2/3 de un metro, en el salón de belleza le cortaron 1/3. ¿Qué tan largo es el cabello de Rosa ahora? ',
             'datos'     : [ '2/3 metro', '1/3 metro' ],
-            'operacion' : [ '2/3', '-', '1/3', '=', '3/9' ],
+            'operacion' : [ '2/3', '-', '1/3', '=', '1/3' ],
             'respuesta' : 'Le queda 3/9 de caja con chocolate',
             'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'Mario posee 6/8 de manzana de tierra, sembró 3/8 de tomates. ¿Cuánto sembró de la manzana?',
+            'datos'     : [ '6/8 manzanas', '3/8 sembrado' ],
+            'operacion' : [ '6/8', '-', '3/8', '=', '3/8' ],
+            'respuesta' : 'Sembró 3/8 de manzana',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'Andrea es guía turística. Dirige excursiones a las isletas de Granada. Su grupo tiene 12 personas y 2/3 son de Guatemala. ¿Cuántas personas son de Guatemala?',
+            'datos'     : [ '12 personas', '2/3 guatemalteco' ],
+            'operacion' : [ '12', '*', '2/3', '=', '8' ],
+            'respuesta' : 'Son 8 guatemaltecos',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Un campesino aró 3/4 de un campo, luego sembró maíz en 1/2 de la parte arada. ¿Qué parte del campo sembró?',
+            'datos'     : [ '3/4  campo', '1/2 maiz' ],
+            'operacion' : [ '3/4', '*', '1/2', '=', '3/8' ],
+            'respuesta' : 'Sembro 3/8 de campo',
+            'signo'     : '*'
+        },
+        {
+            'ejercicio' : 'Un jardinero poda en la cancha de futbol. El primer día poda 3/8 partes de total, el segundo día poda 1/4 ¿cuánto lleva podado en los dos días?',
+            'datos'     : [ '3/8 jardin', '1/4 jardin' ],
+            'operacion' : [ '3/8', '+', '1/4', '=', '5/8' ],
+            'respuesta' : 'Podó 3/8 del jardin',
+            'signo'     : '+'
+        },
+        {
+            'ejercicio' : 'Si un padre le da a su hija Rocío la mitad de una naranja y a su hijo Jorge la cuarta parte de la misma naranja ¿Qué porción de la naranja dio a sus dos hijos?',
+            'datos'     : [ '1/2 naranja', '1/4 naranja' ],
+            'operacion' : [ '1/2', '+', '1/4', '=', '3/4' ],
+            'respuesta' : 'Dió 3/4 de porcion',
+            'signo'     : '+'
+        },
+        {
+            'ejercicio' : 'Don Juan sembró 1/4 de manzana de frijoles y 1/2 manzana de maíz. ¿Cuánto sembró en total?',
+            'datos'     : [ '1/2 frijoles', '1/4 maiz' ],
+            'operacion' : [ '1/2', '+', '1/4', '=', '3/4' ],
+            'respuesta' : 'Sembró 3/4 de manzana',
+            'signo'     : '+'
+        },
+        {
+            'ejercicio' : 'Don Pedro heredó a sus 2 hijos, a Marcos de heredó 1/2 del ganado y a Juan 1/4 del ganado. ¿Cuánto heredó más a Marcos que a Juan?',
+            'datos'     : [ '1/2 ganado', '1/4 ganado' ],
+            'operacion' : [ '1/2', '-', '1/4', '=', '1/4' ],
+            'respuesta' : 'Marcos heredó 1/4 mas que Juan ',
+            'signo'     : '-'
+        },
+        {
+            'ejercicio' : 'Marcia y su familia se fueron de paseo a Granada durante 2 días, el primer día gastaron 1/3 del dinero que llevaban, el segundo día gastaron 1/5. ¿Cuánto gastaron más el primer día que el segundo?',
+            'datos'     : [ '1/3 dinero', '1/5 ganado' ],
+            'operacion' : [ '1/3', '+', '1/5', '=', '2/15' ],
+            'respuesta' : 'El primer dia gasto 2/15 mas',
+            'signo'     : '+'
+        },
+        {
+            'ejercicio' : 'Mi mamá compró un queso que pesaba 3/4 de kilo, si lo partió en porciones de 3/8. ¿Cuántas porciones salieron?',
+            'datos'     : [ '3/4 kilo', '3/8 porciones' ],
+            'operacion' : [ '3/4', '/', '3/8', '=', '2' ],
+            'respuesta' : 'Salieron 2 porciones',
+            'signo'     : '/'
+        },
+        {
+            'ejercicio' : 'Ana ha recorrido 600 metros, que es 1/4 del camino de su casa al colegio. ¿Qué distancia hay de su casa al colegio?',
+            'datos'     : [ '600 metros', '1/4 camino' ],
+            'operacion' : [ '600', '/', '1/4', '=', '2.4' ],
+            'respuesta' : 'Hay 2400  metros',
+            'signo'     : '/'
         }
     ];
 });
